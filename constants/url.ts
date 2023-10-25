6ed2245171963a693586596c9a6a00b3e2c70348
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2023-10-25 20:10:03 +0800

    feat: Fix hostname

--- constants/url.ts
+++ constants/url.ts
@@ -1,9 +1,7 @@
 export const isBrowser = typeof window !== 'undefined';
 
 export const isLocalHost = (): boolean => {
-  // eslint-disable-next-line no-console
-  console.log(location.hostname);
-  return isBrowser ? location.hostname === 'localhost' || location.hostname === 'https://staging.quxpay.com' : false;
+  return isBrowser ? location.hostname === 'localhost' || location.hostname === 'staging.quxpay.com' : false;
 };
 
 export const STAGING_URL = !isLocalHost() ? 'https://api.qux.tv' : 'https://api.quxtech.tv';

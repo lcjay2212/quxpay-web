0c2876752d056c6ad7fd65be0c17c5edb35f41d0
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2024-04-26 05:31:39 +0800

    revert

--- pages/_error.jsx
+++ pages/_error.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import * as Sentry from '@sentry/nextjs';
 import Error from 'next/error';
 

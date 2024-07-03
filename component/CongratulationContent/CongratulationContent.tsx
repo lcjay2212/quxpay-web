7d79102a896f1d27dddd19640fe5cd672d8082e1
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2024-07-03 18:32:07 +0800

    feat: center congratulation content

--- component/CongratulationContent/CongratulationContent.tsx
+++ component/CongratulationContent/CongratulationContent.tsx
@@ -15,82 +15,70 @@ const CongratulationContent: FC<{ label: string }> = ({ label }) => {
   return (
     <>
       {label === 'Purchase' && (
-        <Grid position="relative" justifyContent="center" overflow="hidden">
-          <Box width={430} height={900}>
-            <video
-              height="auto"
-              width="100%"
-              id="backgroud-video"
-              autoPlay
-              loop
-              muted
-              style={{ position: 'fixed', right: 0, bottom: 0, minWidth: '100%', minHeight: '100%' }}
+        <Grid position="relative" overflow="hidden" h="100vh">
+          <video height="auto" width="100%" id="backgroud-video" autoPlay loop muted>
+            <source src="./assets/video/success.mp4" type="video/mp4" />
+          </video>
+          <Container maxW="1080px" position="absolute" mx="auto" left={0} right={0}>
+            <Box
+              pb={{ base: '4rem', md: '5rem' }}
+              display="flex"
+              flexDir="column"
+              alignItems="center"
+              justifyContent="center"
             >
-              <source src="./assets/video/success.mp4" type="video/mp4" />
-            </video>
-          </Box>
-          <Container width={430} position="absolute">
-            <Box>
-              <Box
-                pb={{ base: '4rem', md: '5rem' }}
-                display="flex"
-                flexDir={{ base: 'column', md: 'row', lg: 'row', xl: 'column' }}
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Box mt="14rem">
-                  <Image src={CheckCircleIcon} width={70} height={70} placeholder="empty" alt="Redeem" />
-                </Box>
-                <Heading as="h1" fontWeight="bold" fontSize="24px" color="primary" mt="3rem" mb="1rem">
-                  CONGRATUALATIONS
-                </Heading>
-                <Box color="white" textAlign="center">
-                  <Flex justifyContent="center" mb="1rem" alignItems="center">
-                    <span>
-                      <Image src={QuxTokenIcon} width={40} height={40} alt="Qux Token" />
-                    </span>
-                    <chakra.span fontSize="2rem" mt="0.3rem">
-                      {amount?.toFixed(2)}
-                    </chakra.span>
-                  </Flex>
-                  {type === 'BANK' && (
-                    <>
-                      <Text mb="1rem">
-                        Your transaction has been <br />
-                        Initiated successfully
-                      </Text>
-
-                      <Text>
-                        Please remember that
-                        <br />
-                        bank transactions take
-                        <br />
-                        up to 3 days to process.
-                      </Text>
-                    </>
-                  )}
-                  {type === 'CRYPTO' && (
-                    <Text>
-                      Your transaction has been <br />
-                      Completed successfully
-                    </Text>
-                  )}
-                </Box>
-
-                <Button
-                  variant="primary"
-                  borderRadius="1rem"
-                  mt="16rem"
-                  w={350}
-                  h="3.25rem"
-                  onClick={(): void => {
-                    void router.push('/dashboard');
-                    setVisible(false);
-                  }}
-                >
-                  Complete - Back to Home
-                </Button>
+              <Box mt="14rem">
+                <Image src={CheckCircleIcon} width={70} height={70} placeholder="empty" alt="Redeem" />
               </Box>
+              <Heading as="h1" fontWeight="bold" fontSize="24px" color="primary" mt="3rem" mb="1rem">
+                CONGRATUALATIONS
+              </Heading>
+              <Box color="white" textAlign="center">
+                <Flex justifyContent="center" mb="1rem" alignItems="center">
+                  <span>
+                    <Image src={QuxTokenIcon} width={40} height={40} alt="Qux Token" />
+                  </span>
+                  <chakra.span fontSize="2rem" mt="0.3rem">
+                    {amount?.toFixed(2)}
+                  </chakra.span>
+                </Flex>
+                {type === 'BANK' && (
+                  <>
+                    <Text mb="1rem">
+                      Your transaction has been <br />
+                      Initiated successfully
+                    </Text>
+
+                    <Text>
+                      Please remember that
+                      <br />
+                      bank transactions take
+                      <br />
+                      up to 3 days to process.
+                    </Text>
+                  </>
+                )}
+                {type === 'CRYPTO' && (
+                  <Text>
+                    Your transaction has been <br />
+                    Completed successfully
+                  </Text>
+                )}
+              </Box>
+
+              <Button
+                variant="primary"
+                borderRadius="1rem"
+                mt="16rem"
+                w={350}
+                h="3.25rem"
+                onClick={(): void => {
+                  void router.push('/dashboard');
+                  setVisible(false);
+                }}
+              >
+                Complete - Back to Home
+              </Button>
             </Box>
           </Container>
         </Grid>

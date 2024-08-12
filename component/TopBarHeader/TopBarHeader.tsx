a495cc0176fc86314b4062d71f4c445da8d24f90
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2024-08-12 18:20:27 +0800

    feat: Adjust error message

--- component/TopBarHeader/TopBarHeader.tsx
+++ component/TopBarHeader/TopBarHeader.tsx
@@ -5,12 +5,13 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { QuxPayLogoPng } from 'public/assets';
 import { FC } from 'react';
-import { useHomePageModal } from 'store';
+import { MdOutlineDashboard } from 'react-icons/md';
+import { useHomePageModal, useUser } from 'store';
 
 export const TopBarHeader: FC = () => {
   const router = useRouter();
   const [visible, setVisible] = useHomePageModal(({ visible, setVisible }) => [visible, setVisible]);
-
+  const { user } = useUser();
   return (
     <Container maxW="1080px" bg="transparent" p={0} pr={{ base: '1rem', md: 0 }}>
       <Flex justifyContent="space-between" alignItems="center">
@@ -25,46 +26,58 @@ export const TopBarHeader: FC = () => {
           />
         </Box>
 
-        <Flex gap={4} alignItems="center" display={!visible ? 'flex' : 'none'}>
-          <Button
-            display={{ base: 'none', md: 'block' }}
-            variant="primary"
-            borderRadius="3xl"
-            w={150}
-            h={50}
-            onClick={(): void => void router.push('/login')}
-          >
-            Log In
-          </Button>
-          <Button
-            display={{ base: 'none', md: 'block' }}
-            variant="secondary"
-            borderRadius="3xl"
-            w={150}
-            h={50}
-            onClick={(): void => void router.push('/register')}
-          >
-            Register
-          </Button>
+        {!user ? (
+          <Flex gap={4} alignItems="center" display={!visible ? 'flex' : 'none'}>
+            <Button
+              display={{ base: 'none', md: 'block' }}
+              variant="primary"
+              borderRadius="3xl"
+              w={150}
+              h={50}
+              onClick={(): void => void router.push('/login')}
+            >
+              Log In
+            </Button>
+            <Button
+              display={{ base: 'none', md: 'block' }}
+              variant="secondary"
+              borderRadius="3xl"
+              w={150}
+              h={50}
+              onClick={(): void => void router.push('/register')}
+            >
+              Register
+            </Button>
+            <IconButton
+              mr={{ base: '1rem', md: 0 }}
+              bg="transparent"
+              _active={{ bg: 'transparent' }}
+              _hover={{ bg: 'transparent' }}
+              aria-label="hamburger"
+              icon={
+                <HamburgerIcon
+                  color="purple"
+                  _active={{
+                    color: 'white',
+                  }}
+                  h={35}
+                  w={35}
+                />
+              }
+              onClick={(): void => setVisible(!visible)}
+            />
+          </Flex>
+        ) : (
           <IconButton
             mr={{ base: '1rem', md: 0 }}
             bg="transparent"
             _active={{ bg: 'transparent' }}
             _hover={{ bg: 'transparent' }}
             aria-label="hamburger"
-            icon={
-              <HamburgerIcon
-                color="purple"
-                _active={{
-                  color: 'white',
-                }}
-                h={35}
-                w={35}
-              />
-            }
-            onClick={(): void => setVisible(!visible)}
+            icon={<MdOutlineDashboard style={{ height: '30px', width: '30px' }} color="white" />}
+            onClick={(): void => void router.push('/dashboard')}
           />
-        </Flex>
+        )}
       </Flex>
       <HomeModal />
     </Container>

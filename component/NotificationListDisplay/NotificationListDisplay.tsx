1694de86b2ab93508e2b07839ab8c4961f85d9ca
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2024-09-05 15:51:52 +0800

    feat: refactor

--- component/NotificationListDisplay/NotificationListDisplay.tsx
+++ component/NotificationListDisplay/NotificationListDisplay.tsx
@@ -10,36 +10,34 @@ export const NotificationListDisplay: FC<{
   onClick?: () => void;
 }> = ({ title, date, message, onClick }) => {
   return (
-    <Flex justifyContent="space-between" onClick={onClick} cursor="pointer">
-      <Flex gap={2} justifyContent="flex-start" alignItems="center" color="white">
-        <Flex textAlign="center">
-          <BellIcon color="#FDB851" w="3rem" height="3rem" />
-          <Badge
-            rounded="50%"
-            bg="#DC4143"
-            textAlign="center"
-            mt={1}
-            ml={-5}
-            fontSize="10px"
-            cursor="pointer"
-            px="3px"
-            py="2px"
-            w="4"
-            h="4"
-          />
-        </Flex>
-        <Box fontSize="12px">
-          <Flex justifyContent="space-between">
-            <Text fontSize="14px" fontWeight="semibold" noOfLines={1}>
-              {title}
-            </Text>
-            <Text w={28} fontWeight="semibold" fontSize="12px" textAlign="end">
-              {date}
-            </Text>
-          </Flex>
-          <Text noOfLines={1}>{message}</Text>
-        </Box>
+    <Flex gap={2} justifyContent="flex-start" alignItems="center" color="white" onClick={onClick} cursor="pointer">
+      <Flex textAlign="center">
+        <BellIcon color="#FDB851" w="3rem" height="3rem" />
+        <Badge
+          rounded="50%"
+          bg="#DC4143"
+          textAlign="center"
+          mt={1}
+          ml={-5}
+          fontSize="10px"
+          cursor="pointer"
+          px="3px"
+          py="2px"
+          w="4"
+          h="4"
+        />
       </Flex>
+      <Box fontSize="12px" w="auto">
+        <Flex justifyContent="space-between">
+          <Text fontSize="14px" fontWeight="semibold" noOfLines={1}>
+            {title}
+          </Text>
+          <Text w={28} fontWeight="semibold" fontSize="12px" textAlign="end">
+            {date}
+          </Text>
+        </Flex>
+        <Text noOfLines={1}>{message}</Text>
+      </Box>
     </Flex>
   );
 };
--- constants/api.ts
+++ constants/api.ts
@@ -9,109 +9,89 @@ export const post = async <T>(url: string, variable: void): Promise<T> =>
     },
   });
 
-const token = typeof window !== 'undefined' && localStorage.QUX_PAY_USER_TOKEN;
-
 const getData = async <T>(apiUrl: string, url: string, customToken?: string): Promise<T> => {
+  const token = customToken ?? localStorage.QUX_PAY_USER_TOKEN;
+
   const { data } = await axios.get(`${apiUrl}/${url}`, {
     headers: {
-      Authorization: `Bearer ${customToken ?? token}`,
+      Authorization: `Bearer ${token}`,
       Version: 2,
     },
   });
+
   return data.data;
 };
 
-export const FETCH_WALLET_BALANCE = async (): Promise<any> => {
-  return await getData<any>(STAGING_URL, `web/wallet/balance`, localStorage.QUX_PAY_USER_TOKEN);
+// Generalized fetch function
+const fetchResource = async (endpoint: string): Promise<any> => {
+  return await getData<any>(STAGING_URL, endpoint);
 };
-export const FETCH_TRANSACTION_HISTORY = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/wallet/transactions`, localStorage.QUX_PAY_USER_TOKEN);
-export const FETCH_CRYPTO_TRANSACTION_HISTORY = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/crypto/transactions`, localStorage.QUX_PAY_USER_TOKEN);
-export const FETCH_NOTIFICATION_HISTORY = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/notifications`, localStorage.QUX_PAY_USER_TOKEN);
-export const FETCH_BANK_AND_CREDIT_CARD = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/bankaccount/list`, localStorage.QUX_PAY_USER_TOKEN);
-export const FETCH_FRIEND_LIST = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/friends`, localStorage.QUX_PAY_USER_TOKEN);
-export const FETCH_BANK_LIST = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(
-    STAGING_URL,
-    `web/banks/list?search=${queryKey[1]}${!queryKey[2] ? '' : `&routing_number=${queryKey[2]}`}`,
-    localStorage.QUX_PAY_USER_TOKEN
-  );
-export const SHOW_BANK_ACCOUNT_DETAILS = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(
-    STAGING_URL,
-    `web/wallet/show?payment_profile_id=${queryKey[1]}&payment_type=${queryKey[2]}`,
-    localStorage.QUX_PAY_USER_TOKEN
-  );
 
-export const FETCH_POS_HISTORY = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/pos`, localStorage.QUX_PAY_USER_TOKEN);
+export const FETCH_WALLET_BALANCE = async (): Promise<any> => await fetchResource('web/wallet/balance');
+
+export const FETCH_TRANSACTION_HISTORY = async (): Promise<any> => await fetchResource(`web/wallet/transactions`);
+
+export const FETCH_CRYPTO_TRANSACTION_HISTORY = async (): Promise<any> =>
+  await fetchResource(`web/crypto/transactions`);
+
+export const FETCH_NOTIFICATION_HISTORY = async (): Promise<any> => await fetchResource(`web/notifications`);
+
+export const FETCH_BANK_AND_CREDIT_CARD = async (): Promise<any> => await fetchResource(`web/bankaccount/list`);
+
+export const FETCH_FRIEND_LIST = async (): Promise<any> => await fetchResource(`web/friends`);
+export const FETCH_BANK_LIST = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
+  await fetchResource(`web/banks/list?search=${queryKey[1]}${!queryKey[2] ? '' : `&routing_number=${queryKey[2]}`}`);
+
+export const SHOW_BANK_ACCOUNT_DETAILS = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
+  await fetchResource(`web/wallet/show?payment_profile_id=${queryKey[1]}&payment_type=${queryKey[2]}`);
+
+export const FETCH_POS_HISTORY = async (): Promise<any> => await fetchResource(`web/pos`);
 
 export const FETCH_POS_HISTORY_BY_ID = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/pos/${queryKey[1]}/show`, localStorage.QUX_PAY_USER_TOKEN);
+  await fetchResource(`web/pos/${queryKey[1]}/show`);
 
-export const FETCH_PRODUCT_LIST = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/pos/product/list`, localStorage.QUX_PAY_USER_TOKEN);
+export const FETCH_PRODUCT_LIST = async (): Promise<any> => await fetchResource(`web/pos/product/list`);
 
-export const FETCH_RECENT_PRODUCT_LIST = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/pos/product/recent`, localStorage.QUX_PAY_USER_TOKEN);
+export const FETCH_RECENT_PRODUCT_LIST = async (): Promise<any> => await fetchResource(`web/pos/product/recent`);
 
 export const FETCH_TRANSACTION_HISTORY_PHASE_TWO = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(
-    STAGING_URL,
+  await fetchResource(
     `web/wallet/transactions?${!queryKey[1] ? '' : `date=${queryKey[1]}&`}${
       !queryKey[2] ? '' : `status=${queryKey[2]}&`
-    }${!queryKey[3] ? '' : `transaction_type=${queryKey[3]}`}`,
-    localStorage.QUX_PAY_USER_TOKEN
+    }${!queryKey[3] ? '' : `transaction_type=${queryKey[3]}`}`
   );
 
 export const FETCH_INSIGHTS = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(
-    STAGING_URL,
+  await fetchResource(
     `web/insight?${!queryKey[1] ? '' : `filter_type=${queryKey[1]}&`}${
       !queryKey[2] ? '' : `filter_date=${queryKey[2]}`
-    }`,
-    localStorage.QUX_PAY_USER_TOKEN
+    }`
   );
 
-export const FETCH_BILLING_CATEGORIES = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/billing/categories`, localStorage.QUX_PAY_USER_TOKEN);
+export const FETCH_BILLING_CATEGORIES = async (): Promise<any> => await fetchResource(`web/billing/categories`);
 
 export const FETCH_BILLER_BY_CATEGORY_ID = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(
-    STAGING_URL,
-    `web/billing/billers-by-category?biller_type_id=${queryKey[1]}`,
-    localStorage.QUX_PAY_USER_TOKEN
-  );
+  await fetchResource(`web/billing/billers-by-category?biller_type_id=${queryKey[1]}`);
 
 export const FETCH_BILLER = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
   await getData<any>(STAGING_URL, `web/billing/search/billers?search=${queryKey[1]}`, localStorage.QUX_PAY_USER_TOKEN);
 
 export const FETCH_SCHEDULED_PAYMENT_INFO_BY_ID = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(
-    STAGING_URL,
-    `web/billing/saved-info?saved_payment_info_id=${queryKey[1]}`,
-    localStorage.QUX_PAY_USER_TOKEN
-  );
+  await fetchResource(`web/billing/saved-info?saved_payment_info_id=${queryKey[1]}`);
 
 export const FETCH_SCHEDULED_PAYMENT_LISTS = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/billing/scheduled-payments`, localStorage.QUX_PAY_USER_TOKEN);
+  await fetchResource(`web/billing/scheduled-payments`);
 
 export const FETCH_WP_PO_DETAILS = async ({ queryKey }: QueryFunctionContext): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/wp/po-details?wp=${queryKey[1]}`, localStorage.QUX_PAY_USER_TOKEN);
+  await fetchResource(`web/wp/po-details?wp=${queryKey[1]}`);
 
-export const FETCH_AUTHENTICATION = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/authentication`, localStorage.QUX_PAY_USER_TOKEN);
+export const FETCH_AUTHENTICATION = async (): Promise<any> => await fetchResource(`web/authentication`);
 
 export const FETCH_BANK_CREDIT_CARD_CRYPTO = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/wallet/bank-and-credit`, localStorage.QUX_PAY_USER_TOKEN);
+  await fetchResource(`web/wallet/bank-and-credit`);
 
-export const FETCH_CRYPTO_CURRENCY_LIST = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/crypto/currency`, localStorage.QUX_PAY_USER_TOKEN);
+export const FETCH_CRYPTO_CURRENCY_LIST = async (): Promise<any> => await fetchResource(`web/crypto/currency`);
 
-export const FETCH_CAPTCHA = async (): Promise<any> => await getData<any>(STAGING_URL, `web/captcha`);
-export const FETCH_SECURITY_MAIN_FILE = async (): Promise<any> =>
-  await getData<any>(STAGING_URL, `web/encryption/main-file`);
+export const FETCH_CAPTCHA = async (): Promise<any> => await fetchResource(`web/captcha`);
+
+export const FETCH_SECURITY_MAIN_FILE = async (): Promise<any> => await fetchResource(`web/encryption/main-file`);

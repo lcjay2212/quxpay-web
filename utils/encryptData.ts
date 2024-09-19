22abfb1cccff78348207c21b4d758d949452944b
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2024-09-19 17:00:14 +0800

    feat: change array to object

--- utils/encryptData.ts
+++ utils/encryptData.ts
@@ -22,14 +22,12 @@ export const encryptData = (
 ): { file: string; encrypted_main_key: string; decrypted_main_key: string; iv: string; key: string; type: string } => {
   const masterPublicKey = details.masterPublicKey;
 
-  const innerLayerContent = [
-    {
-      master_public_key: details.details.master_public_key,
-      core: details.details.core,
-      changes: JSON.parse(content),
-      signature: details.details.signature,
-    },
-  ];
+  const innerLayerContent = {
+    master_public_key: details.details.master_public_key,
+    core: details.details.core,
+    changes: JSON.parse(content),
+    signature: details.details.signature,
+  };
 
   const encryptedArray = rsaEncrypt(JSON.stringify(innerLayerContent), masterPublicKey);
 

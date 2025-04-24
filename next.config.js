3cf65026f18574845b8a1e82a314bd3f810d6dbd
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2025-04-24 14:43:17 +0800

    feat: Add

--- next.config.js
+++ next.config.js
@@ -96,6 +96,10 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/_next/static/(.*)',
+        headers: [{ key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }],
+      },
+      {
         source: '/(.*)',
         headers: [
           ...securityHeaders,

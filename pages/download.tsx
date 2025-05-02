c20c19256518ad33543828ac8baf46a02dc31731
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2025-05-02 13:50:32 +0800

    feat: Remove router

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import { Center, Spinner, Text } from '@chakra-ui/react';
-import { useRouter } from 'next/navigation';
 import { FC, useEffect } from 'react';
 
 const DownloadPage: FC = () => {
-  const router = useRouter();
-
   useEffect(() => {
+    if (typeof window === 'undefined' || typeof navigator === 'undefined') return;
+
     const userAgent = navigator.userAgent || navigator.vendor;
 
     if (/android/i.test(userAgent)) {
@@ -15,9 +14,9 @@ const DownloadPage: FC = () => {
     } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
       window.location.href = 'https://apps.apple.com/us/app/quxpay/id6499033621';
     } else {
-      router.replace('https://blog.quxpay.com/downloads/');
+      window.location.href = 'https://blog.quxpay.com/downloads/';
     }
-  }, [router]);
+  }, []);
 
   return (
     <Center h="100vh" flexDirection="column">

12b26c3a55071bc23cc357dd25b981c1ae0f19ca
Christian Jay Paulo Sotto <cjayuu2212@gmail.com>
2025-08-11 20:24:22 +0800

    feat: Hide add bank if user have pending verification

---
+++ hooks/queries/useBankStatus.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import api from 'utils/api';
+
+export const useBankStatus = (): {
+  data: {
+    status: string;
+    account_nickname: string;
+    show_having_trouble: boolean;
+    amounts: number[];
+  };
+  isLoading: boolean;
+} => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['bankStatus'],
+    queryFn: async () => {
+      const { data } = await api.get(`web/verify/bank-status`);
+      return data.data;
+    },
+    refetchOnWindowFocus: false,
+  });
+
+  return {
+    data,
+    isLoading,
+  };
+};
